72d36ef9abf9e186f01c12df40a204817446e5d4
aglowacki <aglowacki.anl@gmail.com>
2017-02-07 12:21:02 -0600

    Added in quick-and-dirty option

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -798,6 +798,165 @@ void generate_optimal_params(std::string dataset_directory,
 
 // ----------------------------------------------------------------------------
 
+void process_dataset_file_quick_n_dirty(std::string dataset_directory,
+                                        std::string dataset_file,
+                                        std::vector<Processing_Type> proc_types,
+                                        ThreadPool* tp,
+                                        std::unordered_map<int, struct params_override> *fit_params_override_dict,
+                                        size_t detector_num_start,
+                                        size_t detector_num_end)
+{
+
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+
+    fitting::models::Gaussian_Model model;
+
+    //Range of energy in spectra to fit
+    fitting::models::Range energy_range;
+    energy_range.min = 0;
+
+    struct params_override * override_params = nullptr;
+    if(override_params == nullptr && fit_params_override_dict->count(-1) > 0)
+    {
+       override_params = &fit_params_override_dict->at(-1);
+    }
+    if(override_params == nullptr)
+    {
+        std::cout<<"Quick and Dirty Skipping file "<<dataset_directory<<" dset "<< dataset_file << " because could not find maps_fit_params_override.txt"<<std::endl;
+        return;
+    }
+
+    //data_struct::xrf::Quantification_Standard * quantification_standard = &(*quant_stand_list)[detector_num];
+
+    std::string full_save_path = dataset_directory+"/img.dat/"+dataset_file+".h5";
+
+    //Spectra volume data
+    data_struct::xrf::Spectra_Volume* spectra_volume = new data_struct::xrf::Spectra_Volume();
+    data_struct::xrf::Spectra_Volume* tmp_spectra_volume = new data_struct::xrf::Spectra_Volume();
+
+    //load the first one
+    size_t detector_num = detector_num_start;
+    if (false == load_spectra_volume(dataset_directory, dataset_file, spectra_volume, detector_num, &override_params->extra_override_values, nullptr, false) )
+    {
+        std::cout<<"Error loading all detectors for "<<dataset_directory<<"/"<<dataset_file<<std::endl;
+        delete spectra_volume;
+        delete tmp_spectra_volume;
+        return;
+    }
+
+    //load spectra volume
+    for(detector_num = detector_num_start+1; detector_num <= detector_num_end; detector_num++)
+    {
+        if (false == load_spectra_volume(dataset_directory, dataset_file, tmp_spectra_volume, detector_num, &override_params->extra_override_values, nullptr, false) )
+        {
+            std::cout<<"Error loading all detectors for "<<dataset_directory<<"/"<<dataset_file<<std::endl;
+            delete spectra_volume;
+            delete tmp_spectra_volume;
+            return;
+        }
+        //add all detectors up
+        for(int j=0; j<spectra_volume->rows(); j++)
+        {
+            for(int k=0; k<spectra_volume->cols(); k++)
+            {
+                real_t elapsed_lifetime = (*spectra_volume)[j][k].elapsed_lifetime();
+                real_t elapsed_realtime = (*spectra_volume)[j][k].elapsed_realtime();
+                real_t input_counts = (*spectra_volume)[j][k].input_counts();
+                real_t output_counts = (*spectra_volume)[j][k].output_counts();
+
+
+                (*spectra_volume)[j][k] += (*tmp_spectra_volume)[j][k];
+
+                elapsed_lifetime += (*tmp_spectra_volume)[j][k].elapsed_lifetime();
+                elapsed_realtime += (*tmp_spectra_volume)[j][k].elapsed_realtime();
+                input_counts += (*tmp_spectra_volume)[j][k].input_counts();
+                output_counts += (*tmp_spectra_volume)[j][k].output_counts();
+
+                (*spectra_volume)[j][k].elapsed_lifetime(elapsed_lifetime);
+                (*spectra_volume)[j][k].elapsed_realtime(elapsed_realtime);
+                (*spectra_volume)[j][k].input_counts(input_counts);
+                (*spectra_volume)[j][k].output_counts(output_counts);
+            }
+        }
+    }
+    delete tmp_spectra_volume;
+
+    energy_range.max = spectra_volume->samples_size() -1;
+
+    for(auto proc_type : proc_types)
+    {
+        //Fit job queue
+        std::queue<std::future<bool> >* fit_job_queue = new std::queue<std::future<bool> >();
+
+        if (override_params->elements_to_fit.size() < 1)
+        {
+            std::cout<<"Error, no elements to fit. Check  maps_fit_parameters_override.txt0 - 3 exist"<<std::endl;
+            continue;
+        }
+
+        //Allocate memeory to save fit counts
+        data_struct::xrf::Fit_Count_Dict  *element_fit_count_dict = generate_fit_count_dict(&override_params->elements_to_fit, spectra_volume->rows(), spectra_volume->cols());
+
+        //Fitting models
+        fitting::routines::Base_Fit_Routine *fit_routine = generate_fit_routine(proc_type);
+
+        //for now we default to true to save iter count, in the future if we change the hdf5 layout we can store it per analysis.
+        bool alloc_iter_count = true;
+
+        //reset model fit parameters to defaults
+        model.reset_to_default_fit_params();
+        //Update fit parameters by override values
+        model.update_fit_params_values(override_params->fit_params);
+
+        if (alloc_iter_count)
+        {
+            //Allocate memeory to save number of fit iterations
+            element_fit_count_dict->emplace(std::pair<std::string, data_struct::xrf::Fit_Counts_Array>(data_struct::xrf::STR_NUM_ITR, data_struct::xrf::Fit_Counts_Array()) );
+            element_fit_count_dict->at(data_struct::xrf::STR_NUM_ITR).resize(spectra_volume->rows(), spectra_volume->cols());
+        }
+
+        //Initialize model
+        fit_routine->initialize(&model, &override_params->elements_to_fit, energy_range);
+
+        for(size_t i=0; i<spectra_volume->rows(); i++)
+        {
+            for(size_t j=0; j<spectra_volume->cols(); j++)
+            {
+                //std::cout<< i<<" "<<j<<std::endl;
+                fit_job_queue->emplace( tp->enqueue(fit_single_spectra, fit_routine, &model, &(*spectra_volume)[i][j], &override_params->elements_to_fit, element_fit_count_dict, i, j) );
+            }
+        }
+
+        //file_job_queue->emplace( tp->enqueue( save_results, full_save_path, save_loc, element_fit_count_dict, fit_job_queue) );
+        save_results( full_save_path, save_loc_map.at(proc_type), element_fit_count_dict, fit_routine, fit_job_queue );
+    }
+
+    real_t energy_offset = 0.0;
+    real_t energy_slope = 0.0;
+    real_t energy_quad = 0.0;
+    data_struct::xrf::Fit_Parameters fit_params = model.fit_parameters();
+    if(fit_params.contains(fitting::models::STR_ENERGY_OFFSET))
+    {
+        energy_offset = fit_params[fitting::models::STR_ENERGY_OFFSET].value;
+    }
+    if(fit_params.contains(fitting::models::STR_ENERGY_SLOPE))
+    {
+        energy_slope = fit_params[fitting::models::STR_ENERGY_SLOPE].value;
+    }
+    if(fit_params.contains(fitting::models::STR_ENERGY_QUADRATIC))
+    {
+        energy_quad = fit_params[fitting::models::STR_ENERGY_QUADRATIC].value;
+    }
+    std::queue<std::future<bool> >* file_job_queue = new std::queue<std::future<bool> >();
+
+    //tp->enqueue( save_volume, full_save_path, spectra_volume, file_job_queue );
+    save_volume( full_save_path, nullptr, spectra_volume, energy_offset, energy_slope, energy_quad, file_job_queue);
+
+
+}
+
+// ----------------------------------------------------------------------------
+
 void process_dataset_file(std::string dataset_directory,
                           std::string dataset_file,
                           std::vector<Processing_Type> proc_types,
@@ -863,7 +1022,7 @@ void process_dataset_file(std::string dataset_directory,
             if (override_params->elements_to_fit.size() < 1)
             {
                 std::cout<<"Error, no elements to fit. Check  maps_fit_parameters_override.txt0 - 3 exist"<<std::endl;
-                return;
+                continue;
             }
 
             //Allocate memeory to save fit counts
@@ -1218,7 +1377,7 @@ void help()
 //    std::cout<<"    us = upstream ion chamber\n";
 //    std::cout<<"    ds = downstream ion chamber\n";
 //    std::cout<<"    sr = sr current. "<<std::endl;
-//    std::cout<<"--quick-and-dirty : Integrate the detector range into 1 spectra. "<<std::endl;
+    std::cout<<"--quick-and-dirty : Integrate the detector range into 1 spectra. "<<std::endl;
     std::cout<<"--optimize-fit-override-params : <int> Integrate the 8 largest mda datasets and fit with multiple params\n"<<
                "  1 = matrix batch fit\n  2 = batch fit without tails\n  3 = batch fit with tails\n  4 = batch fit with free E, everything else fixed"<<std::endl;
     std::cout<<"--optimizer <lmfit, mpfit> : Choose which optimizer to use for --optimize-fit-override-params or matrix fit routine \n"<<std::endl;
@@ -1252,6 +1411,7 @@ int main(int argc, char *argv[])
     std::string quant_standard_filename = "";
     std::string whole_command_line = "";
     bool optimize_fit_override_params = false;
+    bool quick_n_dirty = false;
 
     //dict for override info and elements to fit.
     std::unordered_map<int, struct params_override> fit_params_override_dict;
@@ -1348,7 +1508,7 @@ int main(int argc, char *argv[])
     }
     if( clp.option_exists("--quick-and-dirty"))
     {
-        //TODO:
+        quick_n_dirty = true;
     }
 
     //TODO: add --quantify-only option if you already did the fits and just want to add quantification
@@ -1496,11 +1656,18 @@ int main(int argc, char *argv[])
 
         for(std::string dataset_file : dataset_files)
         {
-            process_dataset_file(dataset_dir, dataset_file, proc_types, &tp, &quant_stand_list, &fit_params_override_dict, detector_num_start, detector_num_end);
+            if(quick_n_dirty)
+            {
+                process_dataset_file_quick_n_dirty(dataset_dir, dataset_file, proc_types, &tp, &fit_params_override_dict, detector_num_start, detector_num_end);
+            }
+            else
+            {
+                process_dataset_file(dataset_dir, dataset_file, proc_types, &tp, &quant_stand_list, &fit_params_override_dict, detector_num_start, detector_num_end);
+            }
         }
     }
 
-    if( clp.option_exists("--generate-avg-h5"))
+    if(quick_n_dirty == false && clp.option_exists("--generate-avg-h5"))
     {
         for(std::string dataset_file : dataset_files)
         {
