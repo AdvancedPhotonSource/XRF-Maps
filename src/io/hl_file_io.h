24eef09ee8b0eba2e129422b2c55c1336e17f414
aglowacki <aglowacki.anl@gmail.com>
2017-05-16 11:01:31 -0500

    Added export to functions in io::hl_file_io.h

--- src/io/hl_file_io.h
+++ src/io/hl_file_io.h
@@ -101,46 +101,46 @@ struct file_name_fit_params
 
 // ----------------------------------------------------------------------------
 
-bool compare_file_size (const file_name_size& first, const file_name_size& second);
+DLL_EXPORT bool compare_file_size (const file_name_size& first, const file_name_size& second);
 
-void populate_netcdf_hdf5_files(std::string dataset_dir);
+DLL_EXPORT void populate_netcdf_hdf5_files(std::string dataset_dir);
 
-bool load_element_info(std::string element_henke_filename,
+DLL_EXPORT bool load_element_info(std::string element_henke_filename,
                        std::string element_csv_filename,
                        data_struct::xrf::Element_Info_Map *element_info_map);
 
 
-bool save_results(std::string save_loc,
+DLL_EXPORT bool save_results(std::string save_loc,
                   const data_struct::xrf::Fit_Count_Dict * const element_counts,
                   fitting::routines::Base_Fit_Routine* fit_routine,
                   std::queue<std::future<bool> >* job_queue,
                   std::chrono::time_point<std::chrono::system_clock> start);
 
-bool save_volume(data_struct::xrf::Quantification_Standard * quantification_standard,
+DLL_EXPORT bool save_volume(data_struct::xrf::Quantification_Standard * quantification_standard,
                  data_struct::xrf::Spectra_Volume *spectra_volume,
                  real_t energy_offset,
                  real_t energy_slope,
                  real_t energy_quad);
 
-void save_optimized_fit_params(struct file_name_fit_params file_and_fit_params);
+DLL_EXPORT void save_optimized_fit_params(struct file_name_fit_params file_and_fit_params);
 
 
-void save_averaged_fit_params(std::string dataset_dir,
+DLL_EXPORT void save_averaged_fit_params(std::string dataset_dir,
                               std::vector<data_struct::xrf::Fit_Parameters> fit_params_avgs,
                               int detector_num_start,
                               int detector_num_end);
 
 
-bool load_quantification_standard(std::string dataset_directory,
+DLL_EXPORT bool load_quantification_standard(std::string dataset_directory,
                                   std::string quantification_info_file,
                                   std::string *standard_file_name,
                                   std::unordered_map<std::string, real_t> *element_standard_weights);
 
-bool load_override_params(std::string dataset_directory,
+DLL_EXPORT bool load_override_params(std::string dataset_directory,
                           int detector_num,
                           data_struct::xrf::Params_Override *params_override);
 
-bool load_spectra_volume(std::string dataset_directory,
+DLL_EXPORT bool load_spectra_volume(std::string dataset_directory,
                          std::string dataset_file,
                          data_struct::xrf::Spectra_Volume *spectra_volume,
                          size_t detector_num,
@@ -148,23 +148,23 @@ bool load_spectra_volume(std::string dataset_directory,
                          data_struct::xrf::Quantification_Standard * quantification_standard,
                          bool save_scalers);
 
-bool load_and_integrate_spectra_volume(std::string dataset_directory,
+DLL_EXPORT bool load_and_integrate_spectra_volume(std::string dataset_directory,
                                        std::string dataset_file,
                                        data_struct::xrf::Spectra *integrated_spectra,
                                        size_t detector_num,
                                        data_struct::xrf::Params_Override * params_override);
 
-void generate_h5_averages(std::string dataset_directory,
+DLL_EXPORT void generate_h5_averages(std::string dataset_directory,
                           std::string dataset_file,
                           ThreadPool* tp,
                           size_t detector_num_start,
                           size_t detector_num_end);
 
-std::vector<std::string> find_all_dataset_files(std::string dataset_directory, std::string search_str);
+DLL_EXPORT std::vector<std::string> find_all_dataset_files(std::string dataset_directory, std::string search_str);
 
-void check_and_create_dirs(std::string dataset_directory);
+DLL_EXPORT void check_and_create_dirs(std::string dataset_directory);
 
-void sort_dataset_files_by_size(std::string dataset_directory, std::vector<std::string> *dataset_files);
+DLL_EXPORT void sort_dataset_files_by_size(std::string dataset_directory, std::vector<std::string> *dataset_files);
 
 }// end namespace io
 
