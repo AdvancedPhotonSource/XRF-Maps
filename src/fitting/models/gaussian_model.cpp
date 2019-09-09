146bd2e6447134ea6a317ef662d8eb9725cb3831
aglowacki <aglowacki.anl@gmail.com>
2019-09-09 16:03:53 -0500

    Fixed graphing bug not displaying second standard

--- src/fitting/models/gaussian_model.cpp
+++ src/fitting/models/gaussian_model.cpp
@@ -312,32 +312,8 @@ const Spectra Gaussian_Model::model_spectrum(const Fit_Parameters * const fit_pa
 
 
 // multicore start
-//    std::vector<std::string> keys;
-//    for (const auto& itr : (*elements_to_fit))
-//    {
-//        if(itr.first == STR_COHERENT_SCT_AMPLITUDE || itr.first == STR_COMPTON_AMPLITUDE)
-//        {
-//            continue;
-//        }
-//        else
-//        {
-//            keys.push_back(itr.first);
-//        }
-//    }
-//#pragma omp parallel for
-//    for (int i=0; i < (int)keys.size(); i++)
-//    {
-//        Spectra tmp = model_spectrum_element(fit_params, elements_to_fit->at(keys[i]), ev);
-//#pragma omp critical
-//        {
-//            agr_spectra += tmp;
-//        }
-//    }
-// multicore end
-	
-
-// single core start
-    for(const auto& itr : (*elements_to_fit))
+    std::vector<std::string> keys;
+    for (const auto& itr : (*elements_to_fit))
     {
         if(itr.first == STR_COHERENT_SCT_AMPLITUDE || itr.first == STR_COMPTON_AMPLITUDE)
         {
@@ -345,9 +321,33 @@ const Spectra Gaussian_Model::model_spectrum(const Fit_Parameters * const fit_pa
         }
         else
         {
-            agr_spectra += model_spectrum_element(fit_params, itr.second, ev);
+            keys.push_back(itr.first);
         }
     }
+#pragma omp parallel for
+    for (int i=0; i < (int)keys.size(); i++)
+    {
+        Spectra tmp = model_spectrum_element(fit_params, elements_to_fit->at(keys[i]), ev);
+#pragma omp critical
+        {
+            agr_spectra += tmp;
+        }
+    }
+// multicore end
+	
+
+// single core start
+//    for(const auto& itr : (*elements_to_fit))
+//    {
+//        if(itr.first == STR_COHERENT_SCT_AMPLITUDE || itr.first == STR_COMPTON_AMPLITUDE)
+//        {
+//            continue;
+//        }
+//        else
+//        {
+//            agr_spectra += model_spectrum_element(fit_params, itr.second, ev);
+//        }
+//    }
 // single core end
 
     agr_spectra += elastic_peak(fit_params, ev, fit_params->at(STR_ENERGY_SLOPE).value);
