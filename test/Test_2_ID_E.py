615f013ca1f3793e7bfc4c570948ac2757edbe7d
Arthur Glowacki <aglowacki.anl@gmail.com>
2020-01-14 20:08:52 -0600

    updates Test_2_ID_E.py to new pybindings

--- test/Test_2_ID_E.py
+++ test/Test_2_ID_E.py
@@ -2,12 +2,18 @@
 #Don't forget to append XRF-Maps/bin directory to PYTHONPATH
 
 import pyxrfmaps as px
+import os
+
+if os.name == 'nt':
+    os_end_char = '\\'
+else:
+    os_end_char = '/'
 
 element_csv_filename = "../reference/xrf_library.csv"
 element_henke_filename = "../reference/henke.xdr"
 
 def load_spectra_vol_and_print():
-	dataset_dir = '2_ID_E_dataset/'
+	dataset_dir = '2_ID_E_dataset' + os_end_char
 	dataset_file = 'axo_std.mda'
 	detector_num = 0
 	sv = px.Spectra_Volume()
@@ -24,11 +30,10 @@ def load_spectra_vol_and_print():
 def run_analysis():
     px.load_element_info(element_henke_filename, element_csv_filename)
     job = px.AnalysisJob()
-    job.dataset_directory = '2_ID_E_dataset/'
+    job.dataset_directory = '2_ID_E_dataset' + os_end_char
     job.dataset_files = px.find_all_dataset_files(job.dataset_directory + "mda/", ".mda")
     job.optimize_dataset_files = job.dataset_files
-    job.detector_num_start = 0
-    job.detector_num_end = 3
+    job.detector_num_arr = [0,1,2,3]
     job.quick_and_dirty = False
     job.fitting_routines = [px.FittingRoutines.ROI, px.FittingRoutines.SVD, px.FittingRoutines.MATRIX, px.FittingRoutines.NNLS]
     job.quantification_standard_filename = 'maps_standardinfo.txt'
