c690153bb50bd3d98fd52f1515159eebba11be95
Arthur Glowacki <aglowacki.anl@gmail.com>
2021-06-29 14:22:43 -0500

    Revert inf/nan check and change lmfit epsilon to mpfit value since mpfit does better perpixel fit than lmfit

--- src/fitting/optimizers/lmfit_optimizer.cpp
+++ src/fitting/optimizers/lmfit_optimizer.cpp
@@ -98,7 +98,7 @@ void general_residuals_lmfit( const real_t *par, int m_dat, const void *data, re
     // Add background
     ud->spectra_model += ud->spectra_background;
     // Remove nan's and inf's
-    //ud->spectra_model = (ArrayXr)ud->spectra_model.unaryExpr([](real_t v) { return std::isfinite(v) ? v : (real_t)0.0; });
+    ud->spectra_model = (ArrayXr)ud->spectra_model.unaryExpr([](real_t v) { return std::isfinite(v) ? v : (real_t)0.0; });
     // Calculate residuals
     for (int i = 0; i < m_dat; i++ )
     {
@@ -149,7 +149,7 @@ LMFit_Optimizer::LMFit_Optimizer() : Optimizer()
 	_options.ftol = 1.0e-15; //LM_USERTOL; // Relative error desired in the sum of squares. Termination occurs when both the actualand predicted relative reductions in the sum of squares are at most ftol.
     _options.xtol = LM_USERTOL; // Relative error between last two approximations. Termination occurs when the relative error between two consecutive iterates is at most xtol.
     _options.gtol = 1.0e-15;  //LM_USERTOL; // Orthogonality desired between fvec and its derivs. Termination occurs when the cosine of the angle between fvec and any column of the Jacobian is at most gtol in absolute value.
-    _options.epsilon = LM_USERTOL; // Step used to calculate the Jacobian, should be slightly larger than the relative error in the user-supplied functions.
+    _options.epsilon = LM_EPSILON; // Step used to calculate the Jacobian, should be slightly larger than the relative error in the user-supplied functions.
     _options.stepbound = (real_t)100.; // Used in determining the initial step bound. This bound is set to the product of stepbound and the Euclidean norm of diag*x if nonzero, or else to stepbound itself. In most cases stepbound should lie in the interval (0.1,100.0). Generally, the value 100.0 is recommended.
     _options.patience = 2000; // Used to set the maximum number of function evaluations to patience*(number_of_parameters+1).
     _options.scale_diag = 1; // If 1, the variables will be rescaled internally. Recommended value is 1.
